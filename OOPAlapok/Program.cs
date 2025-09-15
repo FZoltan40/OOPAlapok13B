18419c9f6cbe0039487eb86ba87e8b6d300567db
FZoltan40 <farkasz@kkszki.hu>
2025-09-15 08:09:21 +0200

    Feladat3 is ready.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -1,27 +1,53 @@
-﻿namespace OOPAlapok
+﻿using System;
+
+namespace OOPAlapok
 {
     public class Szemely
     {
-        private string nev;
-        private int kor;
+        private string _nev;
+        private int _kor;
 
-        public Szemely(string Nev, int Kor)
+        public string Nev
+        {
+            get { return _nev; } //private adattag kiolvasásához
+            set { _nev = value; } // private adattag beállításához
+        }
+
+        public int Kor
+        {
+            get { return _kor; }
+
+            set
+            {
+                if (value > 0)
+                    _kor = value;
+                else
+                    Console.WriteLine("Nem lehet negatív.");
+            }
+        }
+
+        /*public Szemely(string Nev, int Kor)
         {
             nev = Nev;
             kor = Kor;
-        }
+        }*/
 
-        public string Kiir()
+        /*public string Kiir()
         {
             return $"A személy neve: {nev} és életkora: {kor}";
-        }
+        }*/
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely person = new Szemely("Ilona", 44);
-            System.Console.WriteLine(person.Kiir());
+            Szemely person = new Szemely();
+            person.Nev = "Kiss Ilona";
+            person.Kor = 23;
+            Console.WriteLine($"A személy neve: {person.Nev} és {person.Kor} éves.");
+
+            /*Szemely person = new Szemely("Ilona", 44);
+            System.Console.WriteLine(person.Kiir());*/
 
         }
     }
